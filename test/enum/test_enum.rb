f470f1fd2c02585076e7695bbe0a1c6d3e2195a4
Aaron Patterson <aaron.patterson@gmail.com>
2008-08-07 20:17:10 -0700

    testing enums

---
+++ test/enum/test_enum.rb
@@ -0,0 +1,12 @@
+require 'helper'
+
+class EnumTest < ZOMG::Test
+  ENUM = File.join(ASSETS, 'enum')
+  Dir[File.join(ENUM, '*.idl')].each do |file|
+    define_method(:"test_#{file.gsub('.', '_')}_to_sexp") do
+      assert_nothing_raised {
+        ZOMG::IDL::Parser.parse_file(file).to_sexp
+      }
+    end
+  end
+end
